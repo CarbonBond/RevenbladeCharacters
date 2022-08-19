c13eaca5f60f91a8c51a27951b5f5c002e2adbce
carbon <cheeseburgerbbgun@gmail.com>
2022-08-19 15:19:51 -0600

    Added better styling for header and content area

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,57 @@
-<nav>
-  <a href="https://revenblade.com">revenblade.com</a>
-  <a href="/">Characters</a>
-</nav>
+<header>
+  <nav>
+    <a class="firstNav" href="https://revenblade.com">REVENBLADE</a>
+    <a href="/">CHARACTERS</a>
+  </nav>
+</header>
 
-<slot />
+<div class="content">
+  <slot />
+</div>
+
+<style>
+  header {
+    width: 100vw;
+    height: 4.1rem;
+    background-color: hsl(225, 45%, 9%);
+    padding: 0 1em;
+    font-family: Oswald, sans-serif;
+    letter-spacing: 3px;
+    font-weight: bold;
+    font-size: 16px;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+  .firstNav {
+    background-color: hsl(153, 91%, 40%);
+    color: hsl(225, 45%, 9%);
+  }
+  .firstNav:hover {
+    color: hsl(225, 45%, 9%);
+  }
+  nav > a {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 8em;
+    margin: 0 1em;
+    color: hsl(206, 25%, 43%);
+  }
+  nav > a:hover {
+    color: hsl(206, 25%, 63%);
+  }
+
+  .content {
+    padding: 4rem;
+    font-family: Oswald, sans-serif;
+    width: 100%;
+    max-width: 100ch;
+    margin: auto;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,20 @@
 
 <main>
   <div class="flex flex-col">
-    <input type="text" bind:value={searchValue} />
+    <form class="searchContainer">
+
+
+      <input class="searchbar" type="text" bind:value={searchValue} placeholder="SEARCH" />
+    </form >
 
     {#await data}
       <p>...loading</p>
     {:then data}
-      <div class="flex flex-wrap">
+      <div class="characters flex flex-wrap">
         {#each data.characters as character}
-          {#if character.name.toLowerCase().includes(searchValue)}
+          {#if character.name.toLowerCase().includes(searchValue.toLowerCase())}
             <div class="character">
-              <a href="/{character.name}">
+              <a href="/{character.name}" class="flex-center">
                 <img
                   src="/images/{character.name.toLowerCase()}/icon.png"
                   alt={character.name}
@@ -33,6 +37,28 @@
 </main>
 
 <style>
+  .searchContainer {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    margin: 0 0 2em 0;
+  }
+  .searchbar {
+    background-color: inherit;
+    border: solid 2px hsl(206, 25%, 43%);
+    border-radius: 2rem;
+    height: 2rem;
+    padding: 2rem;
+    color: hsl(206, 25%, 75%);
+    font-size: 2rem;
+    letter-spacing: 0.4px;
+    font-weight: bold;
+    flex: 1 0 auto;
+  }
+  .searchbar::placeholder {
+    color: hsl(206, 25%, 75%);
+    text-align: center;
+  }
   main {
     height: 100%;
     width: 100%;
@@ -47,6 +73,12 @@
     flex-wrap: wrap;
   }
 
+  .flex-center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
   .character {
     width: 215px;
     height: 130px;
@@ -55,12 +87,15 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    border: solid 2px black;
   }
-  .character>a{
-
-      width: 100%;
-      height: 100%;
+  .character > a {
+    width: 100%;
+    height: 100%;
+    box-shadow: 0px 3px 4px black, 0px 0px 3px black;
+    color: hsl(206, 25%, 73%);
+    font-size: 3rem;
+    font-weight: bold;
+    letter-spacing: 2px;
+    text-transform: uppercase;
   }
-
 </style>
